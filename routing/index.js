11fbfb44a699cfc6085c307a39afd776f0326f81
p031420004 <p031420004@student.utem.edu.my>
2021-11-07 00:19:44 +0800

    mariadb

--- routing/index.js
+++ routing/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const sekolah = require('./v1/sekolah')
+const country = require('./v1/country')
 
 router.use('/v1/sekolah', sekolah)
-
+router.use('/v1/nation', country)
 
 
 module.exports = router
