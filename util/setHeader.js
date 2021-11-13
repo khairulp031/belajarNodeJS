379680ed1bbf5c76da306f63913beb34b898a39e
p031420004 <p031420004@student.utem.edu.my>
2021-11-14 03:11:49 +0800

    Access-Control-Allow

--- util/setHeader.js
+++ util/setHeader.js
@@ -6,7 +6,9 @@ const nocache = (req, res, next) => {
 const nohttp = (req, res, next) => {
     res.header('X-Content-Type-Options', 'nosniff');
     res.header('X-XSS-Protection', '1; mode=block');
-    res.header('Access-Control-Allow-Origin', 'localhost:*');
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('X-Frame-Options', 'SAMEORIGIN');
     res.header('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
     if (req.headers.token) {
